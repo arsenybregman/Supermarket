e5b5cb91a04f4d0d60a0ee9d9cf22da2ab5dfe4e
Timofey1234567890 <74640949+Timofey1234567890@users.noreply.github.com>
2024-02-15 00:58:13 +0300

    Update style

--- web/main.go
+++ web/main.go
@@ -13,7 +13,13 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	dir := http.Dir("./assets")
+	fs := http.StripPrefix("/assets", http.FileServer(dir))
+	router.PathPrefix("/assets/").Handler(fs) //static load
+
 	router.HandleFunc("/", handler.IndexHandler).Methods(http.MethodGet, http.MethodPost)
+	log.Println("Server Satrt")
 	log.Fatal(http.ListenAndServe(":80", gorillaH.LoggingHandler(os.Stdout, router)))
 
 }
